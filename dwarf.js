79e78b56403860b142f613eacaa202fb61a78258
Seongmin-choi-digipen <80097230+Seongmin-choi-digipen@users.noreply.github.com>
2021-06-15 03:40:41 +0900

    update2

--- dwarf.js
+++ dwarf.js
@@ -1,45 +1,41 @@
 class dwarf{
-    constructor(x, y, dir, speed, mass = 1){
+    constructor(x, y, imageAnimate){
         this.position = new Vec2(x, y);
-        this.velocity = new Vec2( 1, 0 );
-        this.velocity.setLength(speed);
-        this.velocity.setAngle(dir);
-        this.mass = mass;
+        this.velocity = new Vec2( 0, 0 );
+        this.imageAnimate = imageAnimate;
+        this.imageDeltaTime = 0;
     }
 
     update()
     {
         this.position.addTo( this.velocity );
+        this.animateImg();
     }
 
-    angleTo(otherParticle)
-    {
-        const dx = otherParticle.position.x - this.position.x;
-        const dy = otherParticle.position.y - this.position.y;
-        return atan2(dy, dx);
-    }
 
-    distanceTo(otherParticle)
-    {
-        const dx = otherParticle.position.x - this.position.x;
-        const dy = otherParticle.position.y - this.position.y;
-        return sqrt(dx * dx + dy * dy);
-    }
-
-    gravitateTo(otherParticle)
-    {
-        const distance = this.distanceTo(otherParticle);
-        let grav = new Vec2(1, 0);
-
-        grav.setLength(otherParticle.mass / (distance * distance));
-        grav.setAngle(this.angleTo(otherParticle));
-        this.velocity.addTo(grav);
-    }
-    
     draw(image_reference){
         push();
         imageMode(CENTER);
         image(image_reference, this.position.x, this.position.y);
         pop();
     }
+
+    animateImg(){
+        this.imageDeltaTime += deltaTime / 1000;
+        if(this.imageDeltaTime >= 7){
+            image(this.imageAnimate[2], this.position.x, this.position.y);
+            this.imageDeltaTime = 0;
+        }
+        else if(this.imageDeltaTime >= 2 && this.imageDeltaTime <= 7){
+            image(this.imageAnimate[2], this.position.x, this.position.y);            
+        }
+        else if(this.imageDeltaTime >= 1.75 && this.imageDeltaTime <= 2){
+            image(this.imageAnimate[1], this.position.x, this.position.y);
+        }
+        else{
+            image(this.imageAnimate[0], this.position.x, this.position.y);
+        }
+    }
+
+
 }
--- sketch.js
+++ sketch.js
@@ -1,40 +1,82 @@
-const REDDWARF = 'reddwarf'
+
 
 let gImages = [];
-
 let redDwarf;
+let redDwarfImg = [];
+let blueDwarf;
+let blueDwarfImg = [];
 let center;
 
-let A;
+let screen;
+var check = 0;
+let mouse_screen = 0;
 function preload(){
-    gImages[REDDWARF] = loadImage('DwarfImage/RedDwarf1.png');
+    redDwarfImg.push(loadImage('DwarfImage/RedDwarf1.png'));
+    redDwarfImg.push(loadImage('DwarfImage/RedDwarf2.png'));
+    redDwarfImg.push(loadImage('DwarfImage/RedDwarf3.png'));
+    blueDwarfImg.push(loadImage('DwarfImage/BlueDwarf1.png'));
+    blueDwarfImg.push(loadImage('DwarfImage/BlueDwarf2.png'));
+    blueDwarfImg.push(loadImage('DwarfImage/BlueDwarf3.png'));
 }
 
 function setup()
 {
-    createCanvas( 500, windowHeight );
-    center = new dwarf(width/2, 100, 0, 0, 2000);
-    redDwarf = new dwarf(center.position.x, center.position.y, -HALF_PI, 4, 300);
-    A = 0;
+    createCanvas( 600, windowHeight );
+    redDwarf = new dwarf(width/2, 150, redDwarfImg);
+    blueDwarf = new dwarf(width/2 - 80, 150, blueDwarfImg);
+    screen = 0;
 }   
 
 function draw()
 {
     background( 220 );
-    switch(A){
+    switch(screen){
         case 0:
-            console.log("0");
-            
+            rectMode(CENTER);
+            rect(width/2, height/2 + 130, 300, 50);
+            rect(width/2, height/2 + 200, 300, 50);
+            check == 1;
+            if(mouse_screen == 1){
+                mouse_screen = 0;
+                screen = 2;
+            }
+            else if(mouse_screen == 2){
+                mouse_screen
+                screen = 1;
+                check = 0;
+            }
             break;
+        //크레딧
         case 1:
             console.log("1");
             break;
+        //게임 시작
+        case 2:
+            console.log("2");
+            break;
+        //게임 스토리 3~5
+        case 3:
+            redDwarf.update();
+            blueDwarf.update();
+            break;
+        case 4:
+
+            break;
+        case 5:
+            
+            break;
     }
 
-    redDwarf.update();
-    center.update();
 }
 
 function mousePressed(){
-    A += 1;
+    if(check == 0){
+    screen += 1;
+    }
+    if(mouseX > width/2 - 150 && mouseY > height/2 + 105 && mouseX < width/2 + 150 && mouseY < height /2 + 155 && check == 1){
+        mouse_screen = 1;
+    }
+    else if(mouseX > width/2 - 150 && mouseY > height/2 + 175 && mouseX < width/2 + 150 && mouseY < height /2 + 225 && check == 1){
+        mouse_screen = 2;
+    }
 }
